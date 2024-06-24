a154ba42b352a81af3784c0d947dd9716666f2ce
sergei-destinazio <sergei.shevchenko@destinazio.com>
2024-06-24 11:32:30 +0300

    banner-padding
    Adding padding at the top when opening and closing a banner

---
+++ src/js/footer/banner-padding/index.js
@@ -0,0 +1,23 @@
+// Adding padding at the top when opening and closing a banner starts here
+document.addEventListener('DOMContentLoaded', function() {
+  let bannerDisclaimer = document.querySelector('.banner_disclaimer');
+  let pageWrapper = document.querySelector('.page-wrapper');
+
+
+  if (bannerDisclaimer) {
+    let resizeObserver = new ResizeObserver(entries => {
+      for (let entry of entries) {
+        if (entry.target === bannerDisclaimer) {
+          if (pageWrapper) {
+            pageWrapper.style.paddingTop = entry.contentRect.height + 'px';
+          } else {
+            document.body.style.paddingTop = entry.contentRect.height + 'px';
+          }
+        }
+      }
+    });
+
+
+    resizeObserver.observe(bannerDisclaimer);
+  }
+});
