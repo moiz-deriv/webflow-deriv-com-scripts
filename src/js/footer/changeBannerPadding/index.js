dbf37e00daadce9b2accc1e749ad50962c8f94d4
Ali(Ako) Hosseini <ali.hosseini@regentmarkets.com>
2024-06-27 13:45:55 +0800

    fix: change let to const

--- src/js/footer/changeBannerPadding/index.js
+++ src/js/footer/changeBannerPadding/index.js
@@ -1,6 +1,6 @@
 // Adding padding at the top when opening and closing a banner starts here
   const bannerDisclaimer = document.querySelector('.banner_disclaimer');
-  let pageWrapper = document.querySelector('.page-wrapper');
+  const pageWrapper = document.querySelector('.page-wrapper');
 
 
   if (bannerDisclaimer) {
