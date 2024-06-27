44aab0b37ea1e6638aac4c7634501636bc3cf36b
sergei-destinazio <sergei.shevchenko@destinazio.com>
2024-06-27 12:25:16 +0300

    change banner padding update

--- src/js/footer/changeBannerPadding/index.js
+++ src/js/footer/changeBannerPadding/index.js
@@ -1,21 +1,22 @@
 // Adding padding at the top when opening and closing a banner starts here
-  const bannerDisclaimer = document.querySelector('.banner_disclaimer');
-  const pageWrapper = document.querySelector('.page-wrapper');
-
+document.addEventListener('DOMContentLoaded', function() {
+  let bannerDisclaimer = document.querySelector('.banner_disclaimer');
+  let pageWrapper = document.querySelector('.page-wrapper');
 
   if (bannerDisclaimer) {
-    const resizeObserver = new ResizeObserver(entries => {
+    let resizeObserver = new ResizeObserver(entries => {
       for (let entry of entries) {
         if (entry.target === bannerDisclaimer) {
+          let boundingRect = entry.target.getBoundingClientRect();
           if (pageWrapper) {
-            pageWrapper.style.paddingTop = entry.contentRect.height + 'px';
+            pageWrapper.style.paddingTop = boundingRect.height + 'px';
           } else {
-            document.body.style.paddingTop = entry.contentRect.height + 'px';
+            document.body.style.paddingTop = boundingRect.height + 'px';
           }
         }
       }
     });
 
-
     resizeObserver.observe(bannerDisclaimer);
   }
+});
