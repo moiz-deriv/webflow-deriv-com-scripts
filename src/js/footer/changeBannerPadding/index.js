5670ec791ab8decf17615d0c6b200888d45c629d
sergei-destinazio <sergei.shevchenko@destinazio.com>
2024-06-27 18:27:51 +0300

    change banner padding update 2
    Removed domContentLoaded event listener

--- src/js/footer/changeBannerPadding/index.js
+++ src/js/footer/changeBannerPadding/index.js
@@ -1,22 +1,20 @@
 // Adding padding at the top when opening and closing a banner starts here
-document.addEventListener('DOMContentLoaded', function() {
-  const bannerDisclaimer = document.querySelector('.banner_disclaimer');
-  let pageWrapper = document.querySelector('.page-wrapper');
+const bannerDisclaimer = document.querySelector(".banner_disclaimer");
+const pageWrapper = document.querySelector(".page-wrapper");
 
-  if (bannerDisclaimer) {
-    let resizeObserver = new ResizeObserver(entries => {
-      for (let entry of entries) {
-        if (entry.target === bannerDisclaimer) {
-          let boundingRect = entry.target.getBoundingClientRect();
-          if (pageWrapper) {
-            pageWrapper.style.paddingTop = boundingRect.height + 'px';
-          } else {
-            document.body.style.paddingTop = boundingRect.height + 'px';
-          }
+if (bannerDisclaimer) {
+  let resizeObserver = new ResizeObserver((entries) => {
+    for (let entry of entries) {
+      if (entry.target === bannerDisclaimer) {
+        let boundingRect = entry.target.getBoundingClientRect();
+        if (pageWrapper) {
+          pageWrapper.style.paddingTop = boundingRect.height + "px";
+        } else {
+          document.body.style.paddingTop = boundingRect.height + "px";
         }
       }
-    });
+    }
+  });
 
-    resizeObserver.observe(bannerDisclaimer);
-  }
-});
+  resizeObserver.observe(bannerDisclaimer);
+}
