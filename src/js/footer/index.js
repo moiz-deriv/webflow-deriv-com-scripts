60444fd536e02f983ded117a9bb61696384d5bcb
Ali(Ako) Hosseini <ali.hosseini@deriv.com>
2024-07-01 14:56:09 +0800

    feat: import ws utils

--- src/js/footer/index.js
+++ src/js/footer/index.js
@@ -1,5 +1,7 @@
 // All the footer js files should be imported and exported from here
-import "./helper";
+import "./helper"; // always keep this at the top
+import "./websocket" // always keep this at the top
+import "./clientsCountryCookie"
 import "./updateUrlAsPerLanguage";
 import "./footerComponent/index.js";
 import "./dynamicCtaLabels/index.js";
