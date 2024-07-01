1dd33a3d12466fce937f0181650c71373b70ce73
Ali(Ako) Hosseini <ali.hosseini@deriv.com>
2024-07-01 14:59:41 +0800

    fix: send the req if the cookie is not set

--- src/js/footer/clientsCountryCookie/index.js
+++ src/js/footer/clientsCountryCookie/index.js
@@ -1,12 +1,15 @@
-import { setCookie } from "../cookies";
+import { setCookie, getCookieByKey } from "../cookies";
 const message = JSON.stringify({ website_status: 1 });
 const messageType = "website_status";
 
-window
-  .socketMessageSend(message, messageType)
-  .then((response) => {
-    setCookie("clients_country", response.website_status.clients_country, 30);
-  })
-  .catch((error) => {
-    console.error("Error received:", error);
-  });
+const clientsCountry = getCookieByKey(document.cookie, "clients_country");
+if (!clientsCountry) {
+  window
+    .socketMessageSend(message, messageType)
+    .then((response) => {
+      setCookie("clients_country", response.website_status.clients_country, 30);
+    })
+    .catch((error) => {
+      console.error("Error received:", error);
+    });
+}
