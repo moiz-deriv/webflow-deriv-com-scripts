1bfebd6f4247dec9c300b16e46e973d3cfc7fb87
Ali(Ako) Hosseini <ali.hosseini@deriv.com>
2024-07-01 18:47:56 +0800

    feat: use CF location as a backup

--- src/js/footer/clientsCountryCookie/index.js
+++ src/js/footer/clientsCountryCookie/index.js
@@ -1,5 +1,5 @@
 import { setCookie, getCookieByKey } from "../cookies";
-import footerComponent from "../footerComponent";
+import populateFooterComponent from "../footerComponent";
 
 const message = JSON.stringify({ website_status: 1 });
 const messageType = "website_status";
@@ -8,21 +8,35 @@ const clientsCountryFromCookie = getCookieByKey(
   document.cookie,
   "clients_country"
 );
-const CloudflareCountry = Object.fromEntries(
-  (await (await fetch("https://www.cloudflare.com/cdn-cgi/trace")).text())
-    .split("\n")
-    .map((v) => v.split("=", 2))
-).loc; // this gets the clientsCountry from Cloudflare for temporary use.
-if (CloudflareCountry !== clientsCountryFromCookie) {
-  setCookie("clients_country", CloudflareCountry, 30);
-}
-footerComponent();
+
+async function fetchCountryAndPopulateFooter() {
+    try {
+      const response = await fetch("https://www.cloudflare.com/cdn-cgi/trace");
+      const text = await response.text();
+      const CloudflareCountry = Object.fromEntries(
+        text.split("\n").map(v => v.split("=", 2))
+      ).loc;
+  
+      const clientsCountryFromCookie = getCookie("clients_country");
+  
+      if (CloudflareCountry !== clientsCountryFromCookie) {
+        setCookie("clients_country", CloudflareCountry, 30);
+      }
+  
+      populateFooterComponent();
+    } catch (error) {
+      console.error('Error:', error);
+      populateFooterComponent();
+    }
+  }
+
+  fetchCountryAndPopulateFooter()
 window
   .socketMessageSend(message, messageType)
   .then((response) => {
     if (clientsCountryFromCookie !== response.website_status.clients_country)
       setCookie("clients_country", response.website_status.clients_country, 30);
-    footerComponent();
+    populateFooterComponent();
   })
   .catch((error) => {
     console.error("Error received:", error);
