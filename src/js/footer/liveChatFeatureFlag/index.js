e84e78168cd8c259b51955af218e12c07ecfb347
mayuran-deriv-dev96 <balamayuran@regentmarkets.com>
2024-07-16 15:47:29 +0400

    fix: livechat btn hide

--- src/js/footer/liveChatFeatureFlag/index.js
+++ src/js/footer/liveChatFeatureFlag/index.js
@@ -1,10 +1,33 @@
 import { db, ref, onValue } from "./firebaseConfig.js";
 
+let is_live_chat_visible = false;
+let is_whatapp_visible = false;
+
 function fetchChatData() {
   const dataRef = ref(db, "remote_config/deriv-com/chat.json");
   onValue(dataRef, (snapshot) => {
     const data = snapshot.val();
-    console.log(data);
+    is_live_chat_visible = data.live_chat;
+    is_whatapp_visible = data.whatsapp_chat;
+
+    if (!is_whatapp_visible) {
+      const whatapp_icon = document.querySelector(".whatsapp_chat");
+      if (whatapp_icon) {
+        whatapp_icon.style.visibility = "hidden";
+      }
+    }
+    if (!is_live_chat_visible) {
+      const livechat_icon = document.querySelector(".livechatbtn");
+      if (livechat_icon) {
+        livechat_icon.style.visibility = "hidden";
+      }
+    }
+    //on scroll show live chat
+    window.addEventListener("scroll", function () {
+      if (is_live_chat_visible || is_whatapp_visible) {
+        liveChatWrapper?.classList?.remove("hide-element");
+      }
+    });
   });
 }
 
