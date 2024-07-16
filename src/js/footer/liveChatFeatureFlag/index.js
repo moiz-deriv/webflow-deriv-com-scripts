06679216e532ebff1b4b3d50e1291100b5db145a
mayuran-deriv-dev96 <balamayuran@regentmarkets.com>
2024-07-16 16:19:07 +0400

    fix: change logic

--- src/js/footer/liveChatFeatureFlag/index.js
+++ src/js/footer/liveChatFeatureFlag/index.js
@@ -10,22 +10,28 @@ function fetchChatData() {
     is_live_chat_visible = data.live_chat;
     is_whatapp_visible = data.whatsapp_chat;
 
-    if (!is_whatapp_visible) {
-      const whatapp_icon = document.querySelector(".whatsapp_chat");
-      if (whatapp_icon) {
-        whatapp_icon.style.visibility = "hidden";
-      }
+    const whatapp_icon = document.querySelector(".whatsapp_chat");
+    const livechat_icon = document.querySelector(".livechatbtn");
+
+    // Toggle visibility for WhatsApp icon if available
+    if (whatapp_icon) {
+      whatapp_icon.style.visibility = is_whatapp_visible ? "visible" : "hidden";
     }
-    if (!is_live_chat_visible) {
-      const livechat_icon = document.querySelector(".livechatbtn");
-      if (livechat_icon) {
-        livechat_icon.style.visibility = "hidden";
-      }
+
+    // Toggle visibility for Live Chat icon if available
+    if (livechat_icon) {
+      livechat_icon.style.visibility = is_live_chat_visible
+        ? "visible"
+        : "hidden";
     }
+
     //on scroll show live chat
     window.addEventListener("scroll", function () {
       if (is_live_chat_visible || is_whatapp_visible) {
         liveChatWrapper?.classList?.remove("hide-element");
+      }
+      if (is_live_chat_visible && is_whatapp_visible) {
+        liveChatWrapper?.classList?.add("hide-element");
       }
     });
   });
