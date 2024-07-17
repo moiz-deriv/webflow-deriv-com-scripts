adcc3650eadcf9d1abb1cf5841f204ab509b6865
mayuran-deriv-dev96 <balamayuran@regentmarkets.com>
2024-07-17 10:35:22 +0400

    fix: missing import

--- src/js/footer/liveChatFeatureFlag/index.js
+++ src/js/footer/liveChatFeatureFlag/index.js
@@ -4,6 +4,7 @@ let is_live_chat_visible = false;
 let is_whatapp_visible = false;
 
 function fetchChatData() {
+  const liveChatWrapper = document.getElementById("live_chat-wrapper");
   const dataRef = ref(db, "remote_config/deriv-com/chat");
   onValue(dataRef, (snapshot) => {
     const data = snapshot.val();
