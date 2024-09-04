be65eb871c5ea3853829255c7e93fae2c2718ae8
moiz-deriv <moiz@regentmarkets.com>
2024-09-04 11:47:01 +0800

    feat: added ability for referer token to be saved when the partner affiliates page is opened with the required params

---
+++ src/js/footer/partnerAffiliate/index.js
@@ -0,0 +1,17 @@
+import { getCookieByKey, setCookie, deleteCookie } from "../cookies";
+
+export function setReferToken() {
+  if (window.location.pathname == "/partners/affiliate-ib") {
+    const urlObject = new URL(window.location.href);
+    const params = new URLSearchParams(urlObject.search);
+    const referrer = params.get("referrer");
+    const token = params.get("t");
+    if (referrer != null && token != null) {
+      const referrer_token = getCookieByKey(document.cookie, "referrer_token");
+      if (referrer_token !== token) {
+        deleteCookie("referrer_token");
+        setCookie("referrer_token", token, 365);
+      }
+    }
+  }
+}
